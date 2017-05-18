fe594230dcdd654b96ab3a933e522f7e451bf983
Jason Kim <jasonkim3068@gmail.com>
2017-05-18 11:37:01 -0700

    layed out the routes, not functioning thoooo

---
+++ server/api/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// router.use('/schools', require('./school'))
+// router.use('/teachers', require('./teacher'))
+// router.use('/students', require('./student'))
+// router.use('/studentQuestions', require('./studentQuestion'))
+// router.use('/studentAttendance', require('./studentAttendance'))
+// router.use('/classes', require('./class'))
+// router.use('/lectures', require('./lecture'))
+// router.use('/topics', require('./topic'))
+// router.use('/quizzes', require('./quiz'))
+// router.use('/questions', require('./question'))
+// router.use('/answers', require('./answer'))
+
+module.exports = router;
