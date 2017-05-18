1f9a6f04c948853ae750e2703acc9b061bc72310
J6K <JMKANG06@gmail.com>
2017-05-18 16:20:17 -0700

    Beginning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const env = require('gulp-env');
 const gutil = require('gulp-util');
+const Promise = require('bluebird');
+
 const webpack = require('webpack');
-const webpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config.dev');
+const WebpackDevServer = require('webpack-dev-server');
+
+env({
+  file: './.env',
+  type: 'ini'
+});
+
+const db = require('./db/models/schema');
 
