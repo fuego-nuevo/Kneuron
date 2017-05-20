32b76e8eb4899a1d9741e491de66ff4985ed28b0
Jason Kim <jasonkim3068@gmail.com>
2017-05-20 15:18:10 -0700

    added input fields and helpers for my profile page

---
+++ server/api/users.js
@@ -0,0 +1,19 @@
+// const Sequelize = require('sequelize');
+// const User = require('../db/models/index').User;
+// const router = require('express').Router();
+
+
+
+// router.get('/:userToken', (req, res, next) => {
+//   User.findOne({
+//     where: {
+//       userToken: req.params.userToken
+//     }
+//   })
+//     .then(data => {
+//       console.log('User GET Request Successful');
+//       res.status(200).json(data);
+//     })
+// })
+
+// module.exports = router;
