647f6347721e65775569f984cce35ae004890efc
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-20 00:45:12 -0700

    add routing container

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-import { connect }  from 'react-redux';
-import { loginUser } from '../actions/login';
-import NavBar from '../components/navBar';
-import Login from '../components/login';
-import { BrowserRouter } from 'react-router-dom';
-
+import Routing from './routing';
 class App extends Component {
   constructor() {
     super();
   }
   render() {
-    const { dispatch, errorMessage, isAuthenticated } = this.props;
     return (
       <div>
-        <NavBar
-        isAuthenticated={isAuthenticated}
-        errorMessage={errorMessage}
-        dispatch={dispatch}
-        />
+        <Routing />
       </div>
     );
   } 
 }
 
-const mapStateToProps = state => {
-  const { auth } = state
-  const { isAuthenticated, errorMessage } = auth
-
-  return {
-    isAuthenticated,
-    errorMessage
-  }
-}
-
-export default connect(mapStateToProps)(App);
+export default (App);
