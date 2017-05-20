adaa56f781223a42cf50d6b269d88a4c5b5ceb8a
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-19 20:24:43 -0700

    no longer needed

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { renderApp } from '../src/components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reactRoot = window.document.getElementById("root");
+renderApp().then(appComponent => {
+  ReactDOM.render(appComponent, reactRoot);
+});
