6e2a36fb46796136fe0fb4c295f4355fbd161534
J6K <JMKANG06@gmail.com>
2017-05-20 15:32:03 -0700

    [Feat] Please check. I am sure we do not need two of the same rules

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,27 @@ require('dotenv').config()
 
 
 const SRC_DIR = path.resolve(__dirname, './client/src');
-const BUILD_DIR = path.resolve(__dirname, './client/src');
+const BUILD_DIR = path.resolve(__dirname, './static');
 
 module.exports = {
 
   entry: {
-    'app': [
-      './client/src/index',
-    ]
+    app: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/dev-server',
+        `${SRC_DIR}/index.js`, 
+      ],
   },
   output: {
     filename: 'bundle.js',
-    path: BUILD_DIR
+    path: BUILD_DIR,
+    publicPath: '/static'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js[x]?$/,
+        test: /\.js$/,
         loader: 'babel-loader?cacheDirectory',
         include: SRC_DIR,
         exclude: /node_modules/,
@@ -32,22 +36,17 @@ module.exports = {
           ],
         },
       },
-    ],
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader?cacheDirectory',
-        exclude: /node_modules/,
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015', 'react']
-        }
-      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       }
     ]
-    },
-  devtool: 'inline-sourcemap'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'inline-sourcemap',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
