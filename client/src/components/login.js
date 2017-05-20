670aa1b9a3fd1be7929150af6c2551041c0c437a
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-05-20 10:41:36 -0700

    update auth server side and client side fName, lName, email

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -5,11 +5,14 @@ class Login extends Component {
     super();
     this.state = {};
   }
-  
+
   handleClick(event) {
-    const username = this.refs.username
+    const email = this.refs.email
     const password = this.refs.password
-    const creds = { username: username.value.trim(), password: password.value.trim() }
+    const userType = this.refs.userType
+    const fName = this.refs.fName
+    const lName = this.refs.lName
+    const creds = { email: email.value.trim(), password: password.value.trim(), userType: userType.value.trim(), fName: fName.value.trim(), lName: lName.value.trim() }
     this.props.onLoginClick(creds)
   }
 
@@ -18,15 +21,40 @@ class Login extends Component {
     return(
     <div>
         <input type='text'
-        ref='username'
+        ref='fName'
         className="form-control"
-        placeholder='Username'
+        placeholder='First Name'
+        />
+        <input type='text'
+        ref='lName'
+        className="form-control"
+        placeholder='Last Name'
+        />
+        <input type='text'
+        ref='email'
+        className="form-control"
+        placeholder='Email'
         />
         <input type='password'
         ref='password'
         className="form-control"
         placeholder='Password'
         />
+        <input type="radio"
+        ref="userType"
+        name="account"
+        value="teacher"
+        checked>
+          Teacher
+        </input>
+        <br/>
+        <input type="radio"
+        ref="userType"
+        name="account"
+        value="student">
+          Student
+        </input>
+        <br/>
         <button onClick={(event) => this.handleClick(event)} className="btn btn-primary">
           Login
         </button>
--- server/api/teacher.js
+++ server/api/teacher.js
@@ -7,16 +7,20 @@ const saltRounds = 10;
 router.post('/', (req, res, next) => {
   bcrypt.genSalt(saltRounds, function(err, salt){
       bcrypt.hash(req.body.password, salt, function(err, hash){
-        User.findOne({where: {username: req.body.username }}).then((person) => {
+        User.findOne({where: {email: req.body.email, fName: req.body.fName, lName: req.body.lName }}).then((person) => {
           if(person){
-            console.log('That username is taken. Please try another username.');
+            console.log('That email is taken. Please try another email.');
             res.status(404).send(err);
           } else {
             User.create({
-              username: req.body.username,
-              password: hash
+              email: req.body.email,
+              password: hash,
+              userType: req.body.userType,
+              fName: req.body.fName,
+              lName: req.body.lName
             })
             .then((newUser) => {
+              console.log("Signed Up New User: ", newUser);
               res.status(201).send(newUser);
             })
           }
@@ -26,11 +30,12 @@ router.post('/', (req, res, next) => {
 });
 
 //login
-router.get('/:credentials', (req, res, next) => {
-  const userInfo = JSON.parse(req.params.credentials);
-  User.findOne({where: {username: userInfo.username }}).then((user) => {
+router.get('/:email/:creds', (req, res, next) => {
+  // const userInfo = JSON.parse(req.params.handle);
+  console.log(req.params.email);
+  User.findOne({where: {email: req.params.email }}).then((user) => {
     if(user){
-      bcrypt.compare(userInfo.password, user.password, function(err, data){
+      bcrypt.compare(req.params.creds, user.password, function(err, data){
         if(data){
           console.log("User Logged In");
           res.status(200).send(user);
@@ -40,8 +45,10 @@ router.get('/:credentials', (req, res, next) => {
         }
       });
     } else {
-      console.log('Invalid Login Credentials');
-      res.send('User Does Not Exist');
+      console.log('User Does Not Exist');
+      res.status(404).send('User Does Not Exist');
     }
   });
 });
+
+module.exports = router;
