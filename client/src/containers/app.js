103debcdb54e891bcc1111d24f58c71cdda89f55
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-21 13:48:52 -0700

    change order of app, browser router out most component

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Routing from './routing';
 class App extends Component {
   constructor() {
@@ -6,9 +7,9 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <Routing />
-      </div>
+      </BrowserRouter>
     );
   }
 }
