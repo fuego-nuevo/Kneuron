185131268e8cc5864e32dce8e22a52b8fcf4c338
Jason Kim <jasonkim3068@gmail.com>
2017-05-21 23:53:39 -0700

    conflicts

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import Routing from './routing';
 import { connect }  from 'react-redux';
 import { loginUser } from '../actions/login';
-<<<<<<< HEAD
-=======
-import NavBar from '../components/navBar';
->>>>>>> e562a806bb265b1ad0ea94ff90aa68a31c58f7c4
 import UserProfile from '../components/userProfile'
 import Login from '../components/login';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,15 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <Routing />
-=======
-        <NavBar
-        isAuthenticated={isAuthenticated}
-        errorMessage={errorMessage}
-        dispatch={dispatch}
-        />
->>>>>>> e562a806bb265b1ad0ea94ff90aa68a31c58f7c4
         <UserProfile />
       </div>
     );
