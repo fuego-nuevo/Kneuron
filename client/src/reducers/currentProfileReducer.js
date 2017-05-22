8b0e2d699f1545fbad17ed29897f2aca6e40875a
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-22 12:23:47 -0700

    code clean up for redux

--- client/src/reducers/currentProfileReducer.js
+++ client/src/reducers/currentProfileReducer.js
@@ -1,11 +1,11 @@
 
 const currentProfile = (state = {}, action) => {
-    switch(action.type){
-      case 'CURRENT_PROFILE' : 
-        return action.payload;
-      default: 
-        return state;
-    }
-}
+  switch (action.type) {
+    case 'CURRENT_PROFILE' :
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default currentProfile;
