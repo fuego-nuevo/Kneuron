6f5d211f5ad274234efbb52cf0977b59048d2f78
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-22 00:16:00 -0700

    refactor

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,13 +2,11 @@
 // import TopNavBar from './TopNavBar';
 // import SideNav from './SideNav';
 
-const Dashboard = (props) => {
-  return (
-    <div>
+const Dashboard = props => (
+  <div>
       HITS THE DASHBOARD
     </div>
   );
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);*/
 export default Dashboard;
