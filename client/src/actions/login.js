d39ec1a29215bec8110c9c78864c540991316a50
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-23 11:25:14 -0700

    [FIX] -fix login

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
+
 const requestLogin = creds => ({
   type: 'LOGIN_REQUEST',
   isFetching: true,
@@ -101,6 +108,6 @@ exports.logoutUser = () => {
     localStorage.removeItem('id_token');
     localStorage.removeItem('access_token');
     dispatch(receiveLogout());
-    console.log('did you receive logout')
+    console.log('did you receive logout');
   };
 };
