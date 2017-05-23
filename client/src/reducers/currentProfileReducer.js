568f220ace1a1a3dc81968dc91ca89505908d043
Jason Kim <jasonkim3068@gmail.com>
2017-05-22 19:23:34 -0700

    yo y redux so fuckked ?

--- client/src/reducers/currentProfileReducer.js
+++ client/src/reducers/currentProfileReducer.js
@@ -1,8 +1,21 @@
+// import { CURRENT_PROFILE } from '../actions/login';
 
-const currentProfile = (state = {}, action) => {
+const currentProfile = (state = {
+  profile: null,
+  loginComplete: false,
+}, action) => {
+  console.log("this is actoin in reducer!!!!!!!!! ", action)
+  // console.log('told you jason current profile is undefined', CURRENT_PROFILE)
   switch (action.type) {
     case 'CURRENT_PROFILE':
-      return action.payload;
+      // return action.payload;
+      return Object.assign({}, state, {
+        profile: action.payload,
+      });
+    case 'loginComplete':
+    return Object.assign({}, state, {
+      loginComplete: true,
+    })
     default:
       return state;
   }
