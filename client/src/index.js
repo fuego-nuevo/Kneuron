fcbc4db91c3fce8bf0d4ec9331fd4436122a82a9
Jason Kim <jasonkim3068@gmail.com>
2017-05-22 19:23:14 -0700

    can you just work please?

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import App from './containers/app.jsx';
 import Reducers from './reducers/index';
 import thunkMiddleware from 'redux-thunk';
 // import api from './middleware/api';
-
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
+import { createLogger } from 'redux-logger';
+const logger = createLogger({});
+const middleware = [
+  thunkMiddleware,
+  logger,
+]
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 const store = createStoreWithMiddleware(Reducers);
-
+console.log("this is my store on line 13", store)
 ReactDOM.render(
     <Provider store={store}>
       <App />
