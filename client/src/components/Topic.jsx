98b36d6a6c60406a19914b8d9c5a160f16097bfb
J6K <JMKANG06@gmail.com>
2017-05-25 19:47:33 -0700

    [Feat] Basic functionality like delete and create

--- client/src/components/Topic.jsx
+++ client/src/components/Topic.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Topic = props => (
-  <div>
-    <p>{props.topic.name}</p>
-  </div>
-);
+class Topic extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: null,
+    };
 
+    this.handleDeleteTopic = this.handleDeleteTopic.bind(this);
+  }
+
+  handleDeleteTopic(event) {
+    event.preventDefault();
+    this.setState({ id: event.target.value });
+    axios.delete(`/api/topics/${this.state.id}`);
+  }
+
+  handleCreateTopic(event) {
+    event.preventDefault();
+    axios.post(`/api/topics/${lecture_id}`);
+    // this.setState({ topics: [...topics, topic] })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.topic.name}</p>
+        <button onClick={this.handleDeleteTopic} className="delete-class"><img alt="delete" src="https://cdn3.iconfinder.com/data/icons/line/36/cancel-256.png" width="25px" height="25px" /></button>
+      </div>
+    );
+  }
+}
 
 export default Topic;
