b61a6f118a5b2bacfd9d38577fe469dc8a45cb78
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-05-26 22:32:33 -0700

    Missing Lecture Comp Stuff Inputted

--- client/src/components/Lectures/Lecture.jsx
+++ client/src/components/Lectures/Lecture.jsx
@@ -2,26 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Lecture = (props) => {
-  const currentLectureRoute = `/dashboard/lectures${props.selectedLecture}`;
-  return (
-    <div
-      className="cohort-entry animated bounceInUp"
-      onMouseEnter={() => props.handleLectureClick(props.lecture.id)}
-    >
+class Lecture extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {};
+
+    this.deleteLecture = this.deleteLecture.bind(this);
+  }
+
+  async deleteLecture() {
+    try {
+      const removed = await axios.delete(`/api/lectures/${this.props.lecture.id}`);
+      console.log("ERRRRMYGOD: ", removed);
+        this.props.fetchTeacherInfo()
+          .then(() => {
+            this.props.history.push('/dashboard/class');
+          })
+          .catch((err) => {
+            console.log('error with deleting class , ERR: ', err);
+          });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  render(){
+    const currentLectureRoute = `/dashboard/lectures${this.props.lecture.id}`;
+    console.log("THE PROPS FOR LECTURE: ", this.props)
+    return (
       <div
-        id="lecture-entry"
-        className="ch-entry-header">{props.lecture.name}</div>
-      <button className="lecture-button">
-        <Link
-          to={currentLectureRoute}
-          selectedLecture={props.selectedLecture || props.lecture.id}
-        >
-          See Topics
-        </Link>
-      </button>
-    </div>
-  );
-};
+        className="cohort-entry animated bounceInUp"
+        onMouseEnter={() => this.props.handleLectureClick(this.props.lecture.id)}
+      >
+        <div
+          id="lecture-entry"
+          className="ch-entry-header">{props.lecture.name}</div>
+        <button className="lecture-button">
+          <Link
+            to={currentLectureRoute}
+            selectedLecture={props.selectedLecture || props.lecture.id}
+          >
+            See Topics
+          </Link>
+        </button>
+      </div>
+    );
+  };
+}
 
 export default Lecture;
