bf1c6edb7f3f15df750263c101db1f949a30811b
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-29 19:54:48 -0700

    fix dashboard conflicts

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -24,6 +24,7 @@ import AddTopic from '../../components/Topics/AddTopic';
 import AddQuestion from '../Questions/AddQuestion';
 import SearchedDataItemsList from '../../components/SearchedContent/SearchedDataItemsList';
 
+
 const socket = io();
 
 class Dashboard extends Component {
