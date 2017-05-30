4ffd836908a48a88adcddad3f99932eddabb3860
J6K <jmkang06@gmail.com>
2017-05-30 16:00:37 -0700

    [Clean] Clean up and example on how to pass down props

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -156,7 +156,7 @@ class Dashboard extends Component {
         <Route path="/dashboard/livelecture" render={this.renderLiveLecture} />
         <Route path="/dashboard/addClass" render={this.renderAddClass} />
         <Route path="/dashboard/editClass" component={EditClass} />
-        <Route path="/dashboard/addQuiz" render={this.renderAddQuiz} />
+        <Route path="/dashboard/addQuiz" render={this.renderAddQuiz} component={(props) => <Quiz question={props.question} >/>
         <Route path="/dashboard/quiz" render={this.renderQuiz} />
         <Route path="/dashboard/addLecture" render={this.renderAddLecture} />
         <Route path="/dashboard/editLecture" component={EditLecture} />
