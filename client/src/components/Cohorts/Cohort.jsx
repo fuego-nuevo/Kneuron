41f9bf98d9200b76a5a2ba91c4c9bbc4aad476cc
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-31 12:58:13 -0700

    add modal for quiz edit

--- client/src/components/Cohorts/Cohort.jsx
+++ client/src/components/Cohorts/Cohort.jsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../../styles/Main.css';
-import { allLectures, currentLecture } from '../../actions/Lectures';
+import { ModalContainer, ModalDialog } from 'react-modal-dialog';
+import { allLectures } from '../../actions/Lectures';
 
 class Cohort extends Component {
   constructor(props) {
@@ -11,10 +12,13 @@ class Cohort extends Component {
     this.state = {
       subject: '',
       lectures: [],
+      isShowingModal: false,
     };
     this.deleteClass = this.deleteClass.bind(this);
     this.fetchLectures = this.fetchLectures.bind(this);
     this.editClass = this.editClass.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   async deleteClass() {
@@ -53,16 +57,39 @@ class Cohort extends Component {
   fetchLectures() {
     this.props.allLectures(this.props.cohort);
   }
+  handleClick() {
+    this.setState({ isShowingModal: true });
+  }
+  handleClose() {
+    this.setState({ isShowingModal: false });
+  }
 
   render() {
     return (
       <div className="cohort-entry animated bounceInUp" >
+        <div>
+          {
+            this.state.isShowingModal &&
+            <ModalContainer onClose={this.handleClose}>
+              <ModalDialog onClose={this.handleClose}>
+                <h2 className="text-center">Edit your quiz :)</h2>
+              </ModalDialog>
+            </ModalContainer>
+          }
+        </div>
         <div className="ch-entry-header">{this.props.cohort.subject}</div>
         <h3>{this.props.cohort.semester}</h3>
         <h3>{this.props.cohort.time}</h3>
         <button className="lecture-button" onClick={this.fetchLectures}><Link to="/dashboard/lectures">Lectures</Link></button>
         <button onClick={this.deleteClass} className="delete-class"><img alt="delete" src="https://cdn3.iconfinder.com/data/icons/line/36/cancel-256.png" width="25px" height="25px" /></button>
-        <button className="edit-button"><img alt="delete" src="http://simpleicon.com/wp-content/uploads/pencil.png" width="25px" height="25px" /></button>
+        <button
+          onClick={() => {
+            this.handleClick();
+          }}
+          className="edit-button"
+        >
+          <img alt="delete" src="http://simpleicon.com/wp-content/uploads/pencil.png" width="25px" height="25px" />
+        </button>
       </div>
     );
   }
