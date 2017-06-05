0e24f16bd37d2c5bf9b2de0249c3cf11d5c092da
J6K <jmkang06@gmail.com>
2017-06-04 18:01:47 -0700

    [FEAT] -REDIS ONLINE storing frontload data into redis

--- server/api/performance.js
+++ server/api/performance.js
@@ -3,27 +3,32 @@ const router = require('express').Router();
 const redis = require('../db/redis');
 
 router.get('/performanceForEveryStudentForAllCohorts/:user_id', (req, res, next) => {
-  // if (redis.get('performance')) {
-  //   const data = redis.get('performance');
-  //   res.status(202).send(data);
-  // }
-  db.Cohort.findAll({
-    where: {
-      teacher_id: req.params.user_id,
-    },
-    include: [{
-      model: db.StudentCohort,
-      include: [{
-        model: db.User,
-        include: [{
-          model: db.Result,
-        }],
-      }],
-    }],
-  })
+  redis.get(`allPerformance${req.params.user_id}`)
     .then((data) => {
-      // redis.set('performance');
-      res.status(201).send(data);
+      const allPerformanceData = JSON.parse(data);
+      if (allPerformanceData !== null) {
+        res.status(202).send(allPerformanceData);
+      } else {
+        db.Cohort.findAll({
+          where: {
+            teacher_id: req.params.user_id,
+          },
+          include: [{
+            model: db.StudentCohort,
+            include: [{
+              model: db.User,
+              include: [{
+                model: db.Result,
+              }],
+            }],
+          }],
+        })
+          .then((performanceData) => {
+            redis.setex(`allPerformance${req.params.user_id}`, 3600, JSON.stringify(performanceData));
+            res.status(201).send(performanceData);
+          })
+          .catch(next);
+      }
     })
     .catch(next);
 });
