945ab55a0db0348e1ece465440080b777f940bc2
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-06-09 04:50:05 -0700

    [Add] results showing for web app

--- client/src/components/Admin/PerformanceEntry.jsx
+++ client/src/components/Admin/PerformanceEntry.jsx
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 class PerformanceEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      sum: 'no results',
+    };
   }
-
+  componentDidMount() {
+    const { results } = this.props.cohort;
+    let sum = 0;
+    if (results.length) {
+      results.forEach((el) => {
+        console.log(el);
+        sum += el.percentage;
+      });
+      sum /= results.length;
+      this.setState({ sum: sum.toString() });
+    }
+  }
   render() {
     const { subject, semester, results } = this.props.cohort;
+    console.log(this.props);
     return (
       <div className="perf-entry">
         <div className="ent-header">
@@ -18,11 +32,14 @@ class PerformanceEntry extends Component {
         <div className="stats">
           <h4>{semester}</h4>
           <h4>{subject}</h4>
-          <h4>class results</h4>
+          <h4>{this.state.sum}</h4>
+          {/* <h4>{(results.reduce((acc, el) => { console.log(acc); return acc.percentage + el.percentage}, 0) / results.length).toString()}</h4>*/}
         </div>
       </div>
     );
   }
 }
-
+// results.reduce((a, b) => {
+//
+// }, 0);
 export default PerformanceEntry;
