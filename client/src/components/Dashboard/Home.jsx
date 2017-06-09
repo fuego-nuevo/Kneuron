640e00a9ecdd0087bf72a201d62b410d5301f0a6
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-06-09 09:22:02 -0700

    [] add -styles to profile

--- client/src/components/Dashboard/Home.jsx
+++ client/src/components/Dashboard/Home.jsx
@@ -10,17 +10,13 @@ class Home extends Component {
 
   render() {
     const { fName, lName, image, cohort } = this.props.profile;
+    console.log(this.props);
     if (this.props.profile.fName) {
       return (
         <div className="teacher-profile">
           <div className="teacher-header" />
           <img alt="profile" className="profile-image" src={image || 'https://web.usask.ca/images/profile.jpg'} />
           <div className="prof-name"><h1>{fName}  {lName}</h1></div>
-            {/* <img*/}
-            {/* alt="profile" className="profile-image"*/}
-            {/* src={image || 'http://ulbcnj.org/wp-content/uploads/2015/05/placeholder.png'}*/}
-            {/* />*/}
-          {/* <h2>{"# Of Cohort's: " } {cohort.length || 1}</h2>*/}
         </div>
       );
     }
